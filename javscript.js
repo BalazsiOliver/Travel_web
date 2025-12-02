f22dc76d3ca97e75e45fb8cb5effcfafde8df44d
Balázsi Olivér <b.oliver.muka@gmail.com>
2025-12-02 15:09:42 +0100

    new sidebar menu (in progress)

--- javscript.js
+++ javscript.js
@@ -7,3 +7,12 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
