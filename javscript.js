1a3ee9d61725f349fe5c68969d5b06902181057a
Balázsi Olivér <b.oliver.muka@gmail.com>
2025-12-05 07:43:39 +0100

    slide in navbar

--- javscript.js
+++ javscript.js
@@ -1,15 +1,8 @@
 
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
+ 
 }
 
 /* Set the width of the side navigation to 0 */
